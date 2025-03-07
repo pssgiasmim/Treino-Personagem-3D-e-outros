ed1a5e99382575a264c1cfbad1e8212ff3c14b97
pssgiasmim <iasmimpassing@gmail.com>
2025-03-07 16:05:37 -0300

    Alterações de Script

--- Treino personagem 3D e outros/Assets/Animação 3D/Animação.cs
+++ Treino personagem 3D e outros/Assets/Animação 3D/Animação.cs
@@ -4,15 +4,99 @@ using UnityEngine;
 
 public class Animação : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float velociMovimento = 5f; //Velocidade de movimento do personnagem.
+    public float forcaPulo = 5f; //froça  do pulo.
+    public Transform cameraTransform; //Referência a câmera que deve seguir o personagem.
+    private Rigidbody rigidbody; //Armazena o Rigidbody do personagem para aplicar física.
+    private bool isGrounded; //Verifica se o personagem está no chão.
+    private Animator anim;
+
+    //No inicio do jogo vai ser pego o Rigidbody anexado no inspector.
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    // Chama os métodos de movimentação (Move()), pulo (Jump()) e para manter a câmera seguindo o jogador (FollowCamera()).
     void Update()
     {
-        
+        Mover();
+        Pular();
+        SeguirCamera();
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            anim.SetTrigger("Muda");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            anim.SetTrigger("Desmuuda");
+        }
     }
+
+    void Mover()
+    {
+        float MoverX = 0f;
+        float MoverZ = 0f;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoverX = -1f;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            MoverX = 1f;
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            MoverZ = 1f;
+        }
+
+        Vector3 direcaoMovimento = new Vector3(MoverX, 0f, MoverZ).normalized;
+        transform.Translate(direcaoMovimento * velociMovimento * Time.deltaTime, Space.World);
+        //Move o personagem usando Translate(), multiplicando pela moveSpeed e Time.deltaTime para suavizar o movimento.
+
+        float speed = direcaoMovimento.magnitude * velociMovimento;
+        anim.SetFloat("Velocidade", speed);
+        anim.SetInteger("VelocidadeInt", (int)speed);
+
+    }
+
+    void Pular()
+    {
+       if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+       {
+            rigidbody.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            isGrounded = false;
+       }
+        
+       //Verifica se o jogador pressionou a tecla espaço e se está no chão.
+       //Aplica força para cima (Vector3.up * forcaPulo).
+       //Define isGrounded como falso para evitar múltipos pulos no ar.
+    }
+
+    void SeguirCamera()
+    {
+        if (cameraTransform != null)
+        {
+            cameraTransform.position = new Vector3(transform.position.x, cameraTransform.position.y, transform.position.z - 5f);
+        }
+        //Atualiza a posição da câmera para que siga o jogador. E mantém a câmera 5 unidade atrás do personagem no eixo Z.
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+
+        // Detecta colisões do personagem. Se colidir  com algum objeto que a tag ground, permite que o jogador pule novamente.
+    }
+
+
+
+
 }
